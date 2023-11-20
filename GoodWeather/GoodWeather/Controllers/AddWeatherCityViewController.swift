15d9eef6275b395254773e575b205c5ed30ffd53
koala panda <koakoa8823@protonmail.com>
2023-11-20 23:43:02 +0900

    コードの追加

--- GoodWeather/GoodWeather/Controllers/AddWeatherCityViewController.swift
+++ GoodWeather/GoodWeather/Controllers/AddWeatherCityViewController.swift
@@ -9,7 +9,18 @@ import Foundation
 import UIKit
 
 
-class AddWeatherCityViewController: UIViewController{
+class AddWeatherCityViewController: UIViewController {
     
+    @IBOutlet weak var cityNameTextField: UITextField!
+    
+    @IBAction func saveCityButtonPressed() {
+        
+        
+        
+    }
+    
+    @IBAction func close() {
+        self.dismiss(animated: true, completion: nil)
+    }
     
 }
